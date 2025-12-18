c3a2f95152d03cde9560df2296f3829c2f7c5418
Artak Avetyan <artak.avetyan.ext@zeiss.com>
2025-12-18 02:44:29 +0100

    added edge device UI with controls, integrated areg, deploy binaries.

--- apps/multiedge/edgedevice/main.cpp
+++ apps/multiedge/edgedevice/main.cpp
@@ -1,4 +1,25 @@
+﻿/************************************************************************
+ * This file is part of the Areg Edge AI project powered by AREG SDK.
+ * The project contains multiple examples of using Edge AI based on Areg communication framework.
+ *
+ *  Areg Edge AI is available as free and open-source software under the MIT License.
+ *
+ *  For detailed licensing terms, please refer to the LICENSE file included
+ *  with this distribution or contact us at info[at]areg.tech.
+ *
+ *  \copyright   © 2025 Aregtech UG. All rights reserved.
+ *  \file        multiedge/edgedevice/main.cpp
+ *  \ingroup     Areg Edge AI, Edge Device
+ *  \author      Artak Avetyan
+ *  \brief       Edge Device Dialog.
+ *
+ ************************************************************************/
+
+/************************************************************************
+ * Includes
+ ************************************************************************/
 #include "multiedge/edgedevice/edgedevice.hpp"
+#include "areg/appbase/Application.hpp"
 
 #include <QApplication>
 #include <QLocale>
@@ -17,6 +38,8 @@ int main(int argc, char *argv[])
             break;
         }
     }
+
+    Application::initApplication();
 
     a.setApplicationName("Edge Device");
     EdgeDevice w;
